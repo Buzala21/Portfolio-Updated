be2fc3a03721ba6d8ae61f5f85d8d86f796fe4ee
Buzala21 <soso.buzaladze2007i@gmail.com>
2023-12-29 17:55:56 +0400

    Adding HomePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import styles from "./page.module.css"
+import {HiArrowNarrowRight } from 'react-icons/hi'
 
 function page() {
   return (
     <div name="home">
 
-      <div className={styles.mainContainer} >
+      <div className={styles.mainContainer}>
+        <p style={{color:"#F1C305"}} >Hi my name is</p>
+        <h1 className={styles.name}>Soso Buzaladze</h1>
+        <h2 className={styles.dev} >I'm Frontend Developer</h2>
+        <p className={styles.desc} >I'm a Frontend Developer specializing in bullding (and occasionally designing)
+           exceptional digital experiences.focused on Currently,
+         building responsiv websites</p>
+         <div>
+          <button className={styles.but} >View Work
+          <span className={styles.arrow} >
+            <HiArrowNarrowRight style={{marginLeft:"10px"}}/>
+            </span>   
+          </button>
+         </div>
+
 
       </div>
 
---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div name="about" className={styles.mainContainer}>
+
+    </div>
+  )
+}
+
+export default about
